9a5f85970d1f46a5ecbf0244abc7550a75f9cfc2
TokeF <Toke1993@hotmail.com>
2025-08-14 20:23:59 +0200

    add some text about me in the picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,44 @@ export default function Home() {
   return (
     <div>
       <main>
-        <img
-          src="https://cdn.pixabay.com/photo/2015/10/29/14/39/web-1012468_1280.jpg"
-          alt="banner image"
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "300px",
-          }}
-        />
+        <div className="image-container">
+          <img
+            src="https://cdn.pixabay.com/photo/2015/10/29/14/39/web-1012468_1280.jpg"
+            alt="banner image"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "300px",
+              zIndex: -1,
+            }}
+          />
+          <div className="text-white pt-24 flex">
+            <div className="flex-1 flex justify-center">
+              <div>
+                <p className="text-xl">Hi I am Toke</p>
+                <h1 className="text-3xl font-bold">
+                  Freelance Software Engineer
+                </h1>{" "}
+                <p className="text-2xl">
+                  I create{" "}
+                  <span className="text-[#FFE066]">
+                    scalable robust solutions
+                  </span>{" "}
+                  across the stack.
+                </p>
+              </div>
+            </div>
+            <div className="flex-1"></div>
+          </div>
+        </div>
         <div style={{ paddingTop: "600px" }}>
+          <h1>Hello world</h1>
+          <h1>Hello world</h1>
+          <h1>Hello world</h1>
+          <h1>Hello world</h1>
+          <h1>Hello world</h1>
           <h1>Hello world</h1>
           <h1>Hello world</h1>
           <h1>Hello world</h1>
